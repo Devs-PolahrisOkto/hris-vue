19a94e48102c70406f86b09e8b44147ab499c0b1
Philip <philipp@ibenta.com>
2021-04-23 23:59:15 +0800

    Revert "add auth layout" revert 44647e4 This reverts commit 44647e457010b6039936739d5046bd1e29d806bc.

--- src/components/layouts/AuthLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<div class="container mt-6">
-    <div class="columns is-flex flex-wrap-wrap is-justify-content-center">
-        <div class="column is-5">
-            <slot></slot>
-        </div>
-    </div>
-</div>
-</template>
