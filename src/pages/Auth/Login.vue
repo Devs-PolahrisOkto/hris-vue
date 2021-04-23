2e421301af93a533291baecf2c81fdf2dbb24cde
Philip <philipp@ibenta.com>
2021-04-23 23:55:38 +0800

    Revert "create and design login page" revert reverts commit 9d83f65bb1549c30506c72360f8dabeb3405a713.

--- src/pages/Auth/Login.vue
+++ src/pages/Auth/Login.vue
@@ -1,58 +1,11 @@
 <template>
-<auth-layout>
-    <form class="card" @submit.prevent="login">
-        <header class="card-header has-background-light p-2">
-            <p class="card-header-title">
-                <b-icon size="is-small" icon="lock" class="mr-2"></b-icon>
-                <span class="has-text-weight-medium">Login</span>
-            </p>
-        </header>
-        <div class="card-content">
-            <div class="content">
-                <b-field label="E-Mail Address">
-                    <b-input 
-                        v-model="form.email" 
-                        expanded>
-                    </b-input>
-                </b-field>
-                <b-field label="Password">
-                    <b-input type="password"
-                        v-model="form.password"
-                        password-reveal>
-                    </b-input>
-                </b-field>
-                <b-field>
-                    <b-checkbox>Remember Me</b-checkbox>
-                </b-field>
-            </div>
-        </div>
-        <footer class="card-footer is-flex is-flex-direction-column p-5">
-            <button class="button is-primary has-text-weight-semibold" expanded>LOGIN</button>
-            <b-button type="is-ghost" class="has-text-dark mt-2">Forgot Password ?</b-button>
-        </footer>
-    </form>
-</auth-layout>
+<div>
+    login
+</div>
 </template>
 
 <script>
 export default {
-    components: {
-        AuthLayout: () => import("@/components/layouts/AuthLayout.vue"),
-    },
-
-    data() {
-        return {
-            form: {
-                email: '',
-                password: '',
-            }
-        }
-    },
-
-    methods: {
-        login() {
-            console.log(this.form)
-        }
-    }
+    
 }
 </script>
