963619bed2bd81acb1fe8cf951c53d9b3a389057
Philip <philipp@ibenta.com>
2021-05-09 11:07:41 +0800

    add link active and exact link active default class

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,9 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  linkActiveClass: 'active',
+  linkExactActiveClass: 'exact-active',
 });
 
 export default router;
