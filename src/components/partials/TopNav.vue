a5db2af9e14a98fda932104da584e40539eb39af
Philip <philipp@ibenta.com>
2021-05-09 18:42:37 +0800

    replace previous topnav with custom topnav

--- src/components/partials/TopNav.vue
+++ src/components/partials/TopNav.vue
@@ -1,55 +1,32 @@
 <template>
-<b-navbar>
-    <template #brand>
-        <b-navbar-item tag="router-link" :to="{ path: '/' }">
-            <h6 class="is-size-6 has-text-weight-bold">HRIS</h6>
-        </b-navbar-item>
-    </template>
-    <template #start>
-    </template>
+<header 
+    class="header" 
+    :class="{'show': toggleState}"
+    id="header"
+>
+    <div class="header__toggle" @click="toggleSideNav">
+        <i id="header-toggle" class="mdi mdi-menu-open nav__icon" v-show="!toggleState"></i>
+        <i id="header-toggle" class="mdi mdi-backburger nav__icon" v-show="toggleState"></i>
+    </div>
 
-    <template #end> 
-        <b-navbar-item>
-            <b-icon
-                class="px-5"
-                icon="bell"
-                size="is-small">
-            </b-icon>
-        </b-navbar-item>
-        <b-navbar-dropdown>
-            <template #label>
-                <b-icon
-                    class="px-5"
-                    icon="account-circle"
-                    size="is-small">
-                </b-icon>
-                John Doe
-            </template>
-            <b-navbar-item href="#">
-                <b-icon
-                    class="px-5"
-                    icon="account"
-                    size="is-small">
-                </b-icon>
-                My Profile
-            </b-navbar-item>
-            <b-navbar-item href="#">
-                <b-icon
-                    class="px-5"
-                    icon="cog"
-                    size="is-small">
-                </b-icon>
-                Settings
-            </b-navbar-item>
-            <b-navbar-item href="#">
-                <b-icon
-                    class="px-5"
-                    icon="logout"
-                    size="is-small">
-                </b-icon>
-                Logout
-            </b-navbar-item>
-        </b-navbar-dropdown>
-    </template>
-</b-navbar>
+    <div class="header__menu">
+
+    </div>
+</header>
 </template>
+
+<script>
+export default {
+    props: {
+        toggleState: {
+            type: Boolean
+        }
+    },
+
+    methods: {
+        toggleSideNav() {
+            this.$emit('toggle-sidenav');
+        },
+    }
+}
+</script>
