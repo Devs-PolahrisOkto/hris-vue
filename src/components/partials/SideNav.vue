82ccb5ca78ee604f3719080adcaa83e4f9bff666
Philip <philipp@ibenta.com>
2021-05-10 09:05:21 +0800

    add dropdown button

--- src/components/partials/SideNav.vue
+++ src/components/partials/SideNav.vue
@@ -5,12 +5,11 @@
     id="nav-bar"
 >
     <nav class="nav">
-        <div>
-            <router-link to="/" class="nav__logo">
-                <salary-icon></salary-icon>
-                <span class="nav__logo-name">POLAHRIS</span>
-            </router-link>
-
+        <router-link to="/" class="nav__logo">
+            <salary-icon></salary-icon>
+            <span class="nav__logo-name">POLAHRIS</span>
+        </router-link>
+        <div class="nav__menu">
             <div class="nav__list">
                 <router-link to="/dashboard" class="nav__link">
                     <i class="mdi mdi-chart-bar nav__icon"></i>
@@ -22,10 +21,17 @@
                     <span class="nav__name">Employees</span>
                 </router-link>
                 
-                <router-link to="/" class="nav__link">
+                <a class="nav__link collapse" @click="toggleDropdown('dropdown1')">
                     <i class="mdi mdi-calendar-clock nav__icon"></i>
                     <span class="nav__name">Time Record</span>
-                </router-link>
+
+                    <i class="mdi mdi-chevron-down nav__icon collapse__link"></i>
+                    <ul class="collapse__menu" id="dropdown1">
+                        <router-link to="/" class="collapse__sublink">TimeInOut</router-link>
+                        <router-link to="/" class="collapse__sublink">Group</router-link>
+                        <router-link to="/" class="collapse__sublink">Members</router-link>
+                    </ul>
+                </a>
 
                 <router-link to="/" class="nav__link">
                     <i class="mdi mdi-cash-multiple nav__icon"></i>
@@ -37,10 +43,20 @@
                     <span class="nav__name">Reports</span>
                 </router-link>
 
-                <router-link to="/" class="nav__link">
+                <a to="/" class="nav__link collapse" @click="toggleDropdown('dropdown2')">
                     <i class="mdi mdi-cog nav__icon"></i>
                     <span class="nav__name">Settings</span>
-                </router-link>
+
+                    <i class="mdi mdi-chevron-down nav__icon collapse__link"></i>
+                    <ul class="collapse__menu" id="dropdown2">
+                        <router-link to="/" class="collapse__sublink">Company</router-link>
+                        <router-link to="/" class="collapse__sublink">Branch</router-link>
+                        <router-link to="/" class="collapse__sublink">Department</router-link>
+                        <router-link to="/" class="collapse__sublink">Holiday</router-link>
+                        <router-link to="/" class="collapse__sublink">Leaves</router-link>
+                        <router-link to="/" class="collapse__sublink">Loans</router-link>
+                    </ul>
+                </a>
             </div>
         </div>
         <a href="#" class="nav__link">
@@ -62,5 +78,14 @@ export default {
             type: Boolean
         }
     },
+
+    methods: {
+        toggleDropdown(id) {
+            const collapseMenu = document.getElementById(id);
+            collapseMenu.classList.toggle('expand'); 
+            const rotate = collapseMenu.previousElementSibling
+            rotate.classList.toggle('rotate')
+        }
+    }
 }
 </script>
