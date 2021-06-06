4f5ceb34e7e293523ce9ca578849d21682fa1ddb
Philip <philipp@ibenta.com>
2021-06-06 15:01:30 +0800

    import vee-validate and moment.js globally

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -9,3 +9,12 @@ Vue.use(Buefy, {
 
 /** MATERIAL DESIGN ICONS */
 import '@mdi/font/css/materialdesignicons.css';
+
+// import vee-validate
+import { ValidationProvider, ValidationObserver } from 'vee-validate/dist/vee-validate.full.esm';
+Vue.component('ValidationObserver', ValidationObserver);
+Vue.component('ValidationProvider', ValidationProvider);
+
+// import moment
+import moment from 'moment';
+Vue.prototype.$moment = moment;
