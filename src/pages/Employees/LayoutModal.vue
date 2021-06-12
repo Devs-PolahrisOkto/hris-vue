262cf34e6ca4a47438b0587fac8cd88f142c47a8
wickeddd3 <philip@gmail.com>
2021-06-12 23:08:46 +0800

    add layout modal for layout config

---
+++ src/pages/Employees/LayoutModal.vue
@@ -0,0 +1,74 @@
+<template>
+<b-modal 
+    v-model="active" 
+    :width="640"
+    :can-cancel="['x']"
+>
+    <form action="">
+        <div class="modal-card">
+            <header class="modal-card-head is-flex is-justify-content-space-between">
+                <h6 class="is-flex is-align-items-center">
+                    <b-icon icon="grid-large" size="is-small"></b-icon> 
+                    <h6 class="is-size-6 has-text-weight-bold px-3">Layout</h6>
+                </h6>
+                <button
+                    type="button"
+                    class="delete"
+                    @click="$emit('close')"/>
+            </header>
+            <section class="modal-card-body">
+                <h6 class="is-size-6 has-text-weight-light mb-5">Choose preferred layout.</h6>
+                <div class="is-flex is-justify-content-center">
+                    <b-field class="px-3">
+                        <b-radio 
+                            v-model="currentLayout"
+                            @input="$emit('change-layout', currentLayout)"
+                            size="is-medium"
+                            native-value="table"
+                        >
+                            Table Layout
+                        </b-radio>
+                    </b-field>
+                    <b-field class="px-3">
+                        <b-radio 
+                            v-model="currentLayout"
+                            @input="$emit('change-layout', currentLayout)"
+                            size="is-medium"
+                            native-value="grid"
+                        >
+                            Grid Layout
+                        </b-radio>
+                    </b-field>
+                </div>
+            </section>
+            <footer class="modal-card-foot">
+                <b-button
+                    label="Cancel"
+                    @click="$emit('close')" />
+                <b-button
+                    label="Save as default"
+                    type="is-primary" />
+            </footer>
+        </div>
+    </form>
+</b-modal>
+</template>
+
+<script>
+export default {
+    props: {
+        active: {
+            type: Boolean
+        },
+        layout: {
+            type: String
+        }
+    },
+
+    data() {
+        return {
+            currentLayout: this.layout
+        }
+    }
+}
+</script>
