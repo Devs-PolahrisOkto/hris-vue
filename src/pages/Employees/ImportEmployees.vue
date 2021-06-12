196380eb29177be16605672393a1dbb96298eb59
wickeddd3 <philip@gmail.com>
2021-06-12 17:16:17 +0800

    add remove selected row button and functionality

--- src/pages/Employees/ImportEmployees.vue
+++ src/pages/Employees/ImportEmployees.vue
@@ -13,9 +13,16 @@
 
     <!-- Import Employees -->
     <div class="container is-fluid my-5">
-        <div class="is-flex is-justify-content-space-between">
-            <div></div>
-            <div class="is-flex">
+        <div class="is-flex is-justify-content-space-between is-align-items-center is-flex-wrap-wrap">
+            <div class="pb-2">
+                <b-button 
+                    icon-right="delete-forever" 
+                    type="is-danger"
+                    :disabled="selectedData.length === 0"
+                    @click="removeSelectedData"
+                    class="mr-1" />
+            </div>
+            <div class="is-flex is-flex-wrap-wrap">
                 <b-field class="file mr-3">
                     <span class="is-size-6 px-3 py-2" v-if="file">{{ file }}</span> 
                     <b-upload>
@@ -43,6 +50,7 @@
             :data="importedData"
             :columns="columns"
             :sticky-header="stickyHeaders"
+            :checked-rows.sync="selectedData"
             checkable
             sticky-checkbox
             striped
@@ -72,6 +80,7 @@ export default {
         return {
             file: null,
             importedData: [],
+            selectedData: [],
             columns: [
                 {
                     field: "FirstName",
@@ -131,248 +140,6 @@ export default {
                     width: "170px",
                 },
             ],
-            // data: [
-            //     {
-            //         id: 1,
-            //         first_name: "Jesse", 
-            //         last_name: "Simmons" ,
-            //         middle_name: "Sim",
-            //         birthdate: "2016/10/15",
-            //         gender: "Male",
-            //         civil_status: "Single",
-            //         position: "Office Staff",
-            //         department: "Accounting Department",
-            //         employment_type: "Probationary",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            //     {
-            //         id: 2,
-            //         first_name: "John", 
-            //         last_name: "Jacobs",
-            //         middle_name: "Jac",
-            //         birthdate: "2016/12/15",
-            //         gender: "Male",
-            //         civil_status: "Married",
-            //         position: "Accounting Staff",
-            //         department: "Accounting Department",
-            //         employment_type: "Probationary",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            //     {
-            //         id: 3,
-            //         first_name: "Tina", 
-            //         last_name: "Gilbert",
-            //         middle_name: "Gil",
-            //         birthdate: "2016/04/26",
-            //         gender: "Female",
-            //         civil_status: "Married",
-            //         position: "Sales Manager",
-            //         department: "Sales Department",
-            //         employment_type: "Regular",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            //     {
-            //         id: 4,
-            //         first_name: "Clarence", 
-            //         last_name: "Flores",
-            //         middle_name: "Flo",
-            //         birthdate: "2016/04/10",
-            //         gender: "Male",
-            //         civil_status: "Single",
-            //         position: "Accounting Manager",
-            //         department: "Accounting Department",
-            //         employment_type: "Regular",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            //     {
-            //         id: 5,
-            //         first_name: "Anne", 
-            //         last_name: "Lee",
-            //         middle_name: "Tan",
-            //         birthdate: "2016/12/06",
-            //         gender: "Female",
-            //         civil_status: "Single",
-            //         position: "Accounting Supervisor",
-            //         department: "Accounting Department",
-            //         employment_type: "Regular",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            //     {
-            //         id: 6,
-            //         first_name: "Sara", 
-            //         last_name: "Armstrong",
-            //         middle_name: "George",
-            //         birthdate: "2016/09/23",
-            //         gender: "Female",
-            //         civil_status: "Married",
-            //         position: "HR Supervisor",
-            //         department: "HR Department",
-            //         employment_type: "Regular",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            //     {
-            //         id: 7,
-            //         first_name: "Anthony", 
-            //         last_name: "Webb",
-            //         middle_name: "Smith",
-            //         birthdate: "2016/08/30",
-            //         gender: "Male",
-            //         civil_status: "Single",
-            //         position: "Customer Service Agent",
-            //         department: "Sales Department",
-            //         employment_type: "Probationary",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            //     {
-            //         id: 8,
-            //         first_name: "Andrew", 
-            //         last_name: "Greene",
-            //         middle_name: "Jordan",
-            //         birthdate: "2016/11/20",
-            //         gender: "Male",
-            //         civil_status: "Single",
-            //         position: "Office Staff",
-            //         department: "Accounting Department",
-            //         employment_type: "Regular",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            //     {
-            //         id: 9,
-            //         first_name: "Russell", 
-            //         last_name: "White",
-            //         middle_name: "Cole",
-            //         birthdate: "2016/07/13",
-            //         gender: "Male",
-            //         civil_status: "Married",
-            //         position: "Office Staff",
-            //         department: "Sales Department",
-            //         employment_type: "Regular",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            //     {
-            //         id: 10,
-            //         first_name: "Lori", 
-            //         last_name: "Hunter",
-            //         middle_name: "Lamar",
-            //         birthdate: "2016/12/09",
-            //         gender: "Female",
-            //         civil_status: "Single",
-            //         position: "IT Staff",
-            //         department: "IT Department",
-            //         employment_type: "Probationary",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            //     {
-            //         id: 11,
-            //         first_name: "Sara", 
-            //         last_name: "Armstrong",
-            //         middle_name: "George",
-            //         birthdate: "2016/09/23",
-            //         gender: "Female",
-            //         civil_status: "Married",
-            //         position: "HR Supervisor",
-            //         department: "HR Department",
-            //         employment_type: "Regular",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            //     {
-            //         id: 12,
-            //         first_name: "Anthony", 
-            //         last_name: "Webb",
-            //         middle_name: "Smith",
-            //         birthdate: "2016/08/30",
-            //         gender: "Male",
-            //         civil_status: "Single",
-            //         position: "Customer Service Agent",
-            //         department: "Sales Department",
-            //         employment_type: "Probationary",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            //     {
-            //         id: 13,
-            //         first_name: "Andrew", 
-            //         last_name: "Greene",
-            //         middle_name: "Jordan",
-            //         birthdate: "2016/11/20",
-            //         gender: "Male",
-            //         civil_status: "Single",
-            //         position: "Office Staff",
-            //         department: "Accounting Department",
-            //         employment_type: "Regular",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            //     {
-            //         id: 14,
-            //         first_name: "Russell", 
-            //         last_name: "White",
-            //         middle_name: "Cole",
-            //         birthdate: "2016/07/13",
-            //         gender: "Male",
-            //         civil_status: "Married",
-            //         position: "Office Staff",
-            //         department: "Sales Department",
-            //         employment_type: "Regular",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            //     {
-            //         id: 15,
-            //         first_name: "Lori", 
-            //         last_name: "Hunter",
-            //         middle_name: "Lamar",
-            //         birthdate: "2016/12/09",
-            //         gender: "Female",
-            //         civil_status: "Single",
-            //         position: "IT Staff",
-            //         department: "IT Department",
-            //         employment_type: "Probationary",
-            //         sss: "111-1111111111",
-            //         philhealth: "12-23322342",
-            //         hdmf: "23432-234234234-23",
-            //         tin: "0000-234234234",
-            //     },
-            // ],
             stickyHeaders: true,
         }
     },
@@ -389,7 +156,6 @@ export default {
                 var worksheet_sheet1 = workbook.SheetNames[0];
                 var worksheet = workbook.Sheets[worksheet_sheet1];
                 var worksheet_data = XLSX.utils.sheet_to_json(worksheet, {raw: false})
-                console.log(worksheet_data)
                 this.importedData = worksheet_data;
                 this.$refs.fileInput.value = "";
             };
@@ -435,6 +201,12 @@ export default {
                 type:"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             }), `Employees Template.${excel_format}`);
         },
+        removeSelectedData() {
+            this.selectedData.forEach(item => {
+                this.importedData.splice(this.importedData.indexOf(item), 1);
+            });
+            this.selectedData = [];
+        }
     }
 }
 </script>
