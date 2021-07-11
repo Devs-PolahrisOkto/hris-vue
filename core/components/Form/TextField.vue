61311be885351393b3ca5bf9d8f17d910a7e32ed
wickeddd3 <pablophilipandrew@gmail.com>
2021-07-12 00:01:58 +0800

    add reusable text field component

---
+++ core/components/Form/TextField.vue
@@ -0,0 +1,33 @@
+<template>
+<ValidationProvider 
+    :name="label" 
+    :rules="rules" 
+    :mode="mode"
+    v-slot="{ errors, valid }"
+>
+    <b-field 
+        :label="label"
+        :label-position="labelPosition" 
+        :type="{ 'is-danger': errors[0], 'is-success': valid }" 
+        :message="errors"
+    >
+        <b-input 
+            :value="value"
+            @input.native="$emit('input', $event.target.value)"
+        ></b-input>
+    </b-field>
+</ValidationProvider>
+</template>
+
+<script>
+export default {
+    props: {
+        label: String,
+        value: String,
+        labelPosition: String,
+        mode: String,
+        rules: String,
+    },
+}
+</script>
+
