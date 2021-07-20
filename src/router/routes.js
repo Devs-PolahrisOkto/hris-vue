558141aaa978c2fc90b5b443b6b8e46761880a81
wickeddd3 <pablophilipandrew@gmail.com>
2021-07-20 22:03:56 +0800

    add meta on routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,46 @@
 const routes = [
-    { path: '/', name: 'Welcome', component: () => import(/* webpackChunkName: "Welcome" */ '@/pages/Welcome/Welcome.vue') },
-    { path: '*', name: 'NotFound', component: () => import(/* webpackChunkName: "NotFound" */ '@/pages/NotFound/NotFound.vue') },
-    { path: '/login', name: 'Login', component: () => import(/* webpackChunkName: "Login" */ '@/pages/Auth/Login.vue') },
-    { path: '/dashboard', name: 'Dashboard', component: () => import(/* webpackChunkName: "Dashboard" */ '@/pages/Dashboard/Dashboard.vue') },
-
-    { path: '/employees', name: 'AllEmployees', component: () => import(/* webpackChunkName: "AllEmployees" */ '@/pages/Employees/AllEmployees.vue') },
-    { path: '/employees/register', name: 'RegisterEmployee', component: () => import(/* webpackChunkName: "RegisterEmployee" */ '@/pages/Employees/RegisterEmployee.vue') },
-    { path: '/employees/import', name: 'ImportEmployees', component: () => import(/* webpackChunkName: "ImportEmployees" */ '@/pages/Employees/ImportEmployees.vue') },
+    { 
+        path: '/', 
+        name: 'Welcome', 
+        component: () => import(/* webpackChunkName: "Welcome" */ '@/pages/Welcome/Welcome.vue'),
+        meta: { forVisitors: true }
+    },
+    { 
+        path: '/login', 
+        name: 'Login', 
+        component: () => import(/* webpackChunkName: "Login" */ '@/pages/Auth/Login.vue'),
+        meta: { forVisitors: true }
+    },
+    { 
+        path: '*', 
+        name: 'NotFound', 
+        component: () => import(/* webpackChunkName: "NotFound" */ '@/pages/NotFound/NotFound.vue'),
+        meta: { forAuth: true }
+    },
+    { 
+        path: '/dashboard', 
+        name: 'Dashboard', 
+        component: () => import(/* webpackChunkName: "Dashboard" */ '@/pages/Dashboard/Dashboard.vue'),
+        meta: { forAuth: true }
+    },
+    { 
+        path: '/employees', 
+        name: 'AllEmployees', 
+        component: () => import(/* webpackChunkName: "AllEmployees" */ '@/pages/Employees/AllEmployees.vue'),
+        meta: { forAuth: true }
+    },
+    { 
+        path: '/employees/register', 
+        name: 'RegisterEmployee', 
+        component: () => import(/* webpackChunkName: "RegisterEmployee" */ '@/pages/Employees/RegisterEmployee.vue'),
+        meta: { forAuth: true }
+    },
+    { 
+        path: '/employees/import', 
+        name: 'ImportEmployees', 
+        component: () => import(/* webpackChunkName: "ImportEmployees" */ '@/pages/Employees/ImportEmployees.vue'),
+        meta: { forAuth: true }
+    },
 ];
 
 export default routes;
