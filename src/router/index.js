2fb99ec79ba04d59cff52dbae452fe0a92b5a6b2
wickeddd3 <pablophilipandrew@gmail.com>
2021-07-20 22:04:27 +0800

    add route guard

--- src/router/index.js
+++ src/router/index.js
@@ -12,4 +12,27 @@ const router = new VueRouter({
   linkExactActiveClass: 'exact-active',
 });
 
+// Route Guard
+router.beforeEach((to, from, next) => {
+  if (to.meta.forAuth) {
+    const authToken = window.localStorage.getItem('authToken');
+    const authUser = JSON.parse(window.localStorage.getItem('authUser'));
+    if (authToken && authUser) {
+        next()
+    } else {
+        next({name: 'Login'})
+    }
+  } else if (to.meta.forVisitors) {
+    const authToken = window.localStorage.getItem('authToken');
+    const authUser = JSON.parse(window.localStorage.getItem('authUser'))
+    if (!authToken || !authUser) {
+        next()
+    } else {
+        next({name: 'Dashboard'})
+    }
+  } else {
+    next()
+  }
+});
+
 export default router;
