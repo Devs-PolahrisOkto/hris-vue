04178bb2ddb186b17ce3b89b53c4d11aa0cbb03c
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-10 00:59:01 +0800

    use text field component on login form and add form validation

--- src/pages/Auth/Login.vue
+++ src/pages/Auth/Login.vue
@@ -1,38 +1,41 @@
 <template>
 <auth-layout>
-    <form class="card" @submit.prevent="handleLogin">
-        <header class="card-header has-background-light p-2">
-            <p class="card-header-title">
-                <span>
-                    <b-icon size="is-small" icon="lock" class="mr-2"></b-icon>
-                    <span class="has-text-weight-medium">Login</span>
-                </span>
-            </p>
-        </header>
-        <div class="card-content">
-            <div class="content">
-                <b-field label="E-Mail Address">
-                    <b-input 
-                        v-model="form.email" 
-                        expanded>
-                    </b-input>
-                </b-field>
-                <b-field label="Password">
-                    <b-input type="password"
+    <ValidationObserver ref="observer" v-slot="{ passes }">
+        <form class="card" @submit.prevent="passes(handleLogin)">
+            <header class="card-header has-background-light p-2">
+                <p class="card-header-title">
+                    <span>
+                        <b-icon size="is-small" icon="lock" class="mr-2"></b-icon>
+                        <span class="has-text-weight-medium">Login</span>
+                    </span>
+                </p>
+            </header>
+            <div class="card-content">
+                <div class="content">
+                    <text-field
+                        label="Email"
+                        v-model="form.email"
+                        rules="required"
+                        mode="eager"
+                    ></text-field>
+                    <text-field
+                        label="Password"
+                        type="password"
                         v-model="form.password"
-                        password-reveal>
-                    </b-input>
-                </b-field>
-                <b-field>
-                    <b-checkbox>Remember Me</b-checkbox>
-                </b-field>
+                        rules="required"
+                        mode="eager"
+                    ></text-field>
+                    <b-field>
+                        <b-checkbox>Remember Me</b-checkbox>
+                    </b-field>
+                </div>
             </div>
-        </div>
-        <footer class="card-footer is-flex is-flex-direction-column p-5">
-            <button class="button is-primary has-text-weight-semibold" expanded>LOGIN</button>
-            <b-button type="is-ghost" class="has-text-dark mt-2">Forgot Password ?</b-button>
-        </footer>
-    </form>
+            <footer class="card-footer is-flex is-flex-direction-column p-5">
+                <button class="button is-primary has-text-weight-semibold" expanded>LOGIN</button>
+                <b-button type="is-ghost" class="has-text-dark mt-2">Forgot Password ?</b-button>
+            </footer>
+        </form>
+    </ValidationObserver>
 </auth-layout>
 </template>
 
