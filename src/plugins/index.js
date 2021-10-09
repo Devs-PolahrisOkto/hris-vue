f21bc38be848d85e045d73f50c72381f8d865017
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-09 23:50:46 +0800

    breakdown plugins into dedicated js files

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,20 +1,4 @@
-/** VUE */
-import Vue from 'vue';
-
-/** BUEFY */
-import Buefy from 'buefy';
-Vue.use(Buefy, {
-    defaultIconPack: 'mdi',
-});
-
-/** MATERIAL DESIGN ICONS */
-import '@mdi/font/css/materialdesignicons.css';
-
-// import vee-validate
-import { ValidationProvider, ValidationObserver } from 'vee-validate/dist/vee-validate.full.esm';
-Vue.component('ValidationObserver', ValidationObserver);
-Vue.component('ValidationProvider', ValidationProvider);
-
-// import moment
-import moment from 'moment';
-Vue.prototype.$moment = moment;
+import '@/plugins/buefy.js';
+import '@/plugins/material-design-icons.js';
+import '@/plugins/vee-validate.js';
+import '@/plugins/moment.js';
---
+++ src/plugins/vee-validate.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import { ValidationProvider, ValidationObserver } from 'vee-validate/dist/vee-validate.full.esm';
+Vue.component('ValidationObserver', ValidationObserver);
+Vue.component('ValidationProvider', ValidationProvider);
