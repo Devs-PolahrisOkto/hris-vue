d85755265e073d117134331d11a22cd2bf227a84
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-11 01:31:55 +0800

    add employee contacts table

--- src/components/Employee/Personal/Contacts.vue
+++ src/components/Employee/Personal/Contacts.vue
@@ -1,3 +1,82 @@
 <template>
-<div>contacts</div>
+<div class="card mb-4">
+    <header class="is-flex is-justify-content-space-between is-align-items-center py-2 pr-2">
+        <h6 class="is-size-6 has-text-weight-light px-3">Contacts</h6>
+        <b-button size="is-small" icon-right="refresh" />
+    </header>
+    <b-table
+        :data="isEmpty ? [] : contacts"
+        :striped="isStriped"
+        :hoverable="isHoverable"
+        :mobile-cards="hasMobileCards"
+        :paginated="isPaginated"
+        :per-page="perPage"
+        :current-page.sync="currentPage"
+        :pagination-simple="isPaginationSimple"
+        :pagination-position="paginationPosition"
+        :default-sort-direction="defaultSortDirection"
+        :pagination-rounded="isPaginationRounded"
+        :sort-icon="sortIcon"
+        :sort-icon-size="sortIconSize"
+        default-sort="school"
+        aria-next-label="Next page"
+        aria-previous-label="Previous page"
+        aria-page-label="Page"
+        aria-current-label="Current page"
+    >
+        <b-table-column field="name" label="Name" sortable v-slot="props">
+            {{ props.row.name }}
+        </b-table-column>
+
+        <b-table-column field="description" label="Description" sortable v-slot="props" width="240">
+            {{ props.row.description }}
+        </b-table-column>
+
+        <template #empty>
+            <div class="has-text-centered">No records</div>
+        </template>
+
+    </b-table>
+</div>
 </template>
+
+<script>
+export default {
+    props: {
+        contacts: {
+            type: Array,
+            default: () => []
+        }
+    },
+
+    data() {
+        return {
+            isEmpty: false,
+            isStriped: true,
+            isHoverable: true,
+            hasMobileCards: true,
+            isPaginated: false,
+            isPaginationSimple: true,
+            isPaginationRounded: false,
+            paginationPosition: 'bottom',
+            defaultSortDirection: 'asc',
+            sortIcon: 'arrow-up',
+            sortIconSize: 'is-small',
+            currentPage: 1,
+            perPage: 10,
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+/deep/ .table td, 
+/deep/ .table th {
+    border: none;
+}
+
+/deep/ .pagination {
+    padding-bottom: 10px;
+    padding-right: 10px;
+}
+</style>
