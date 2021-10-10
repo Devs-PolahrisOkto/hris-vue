b523836aaae744ec5d6f0ac87a044cbde987fd03
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-10 23:55:11 +0800

    import educationalbackground, addresses, and contacts component to personal data component

--- src/components/Employee/Personal/PersonalData.vue
+++ src/components/Employee/Personal/PersonalData.vue
@@ -1,5 +1,23 @@
 <template>
 <div>
-    personal data
+    <educational-background></educational-background>
+    <addresses-table></addresses-table>
+    <contacts-table></contacts-table>
 </div>
 </template>
+
+<script>
+export default {
+    components: {
+        EducationalBackground: () => import("@/components/Employee/Personal/EducationalBackground.vue"),
+        AddressesTable: () => import("@/components/Employee/Personal/Addresses.vue"),
+        ContactsTable: () => import("@/components/Employee/Personal/Contacts.vue"),
+    },
+
+    data() {
+        return {
+            activeTab: 0
+        }
+    }
+}
+</script>
