14a768d6bdcc06cc6abeaeb86b9163141a0c6a31
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-11 02:03:30 +0800

    add company page

---
+++ src/pages/Settings/Company.vue
@@ -0,0 +1,26 @@
+<template>
+<main-layout>
+
+<!-- Start Breadcrumb -->
+<nav class="breadcrumb px-5" aria-label="breadcrumbs">
+  <ul class="px-3 pt-3">
+    <li><a href="#">Admin</a></li>
+    <li class="is-active"><a href="#" aria-current="page">Companies</a></li>
+  </ul>
+</nav>
+<!-- End Breadcrumb -->
+
+<div class="container is-fluid my-5">
+    companies
+</div>
+
+</main-layout>
+</template>
+
+<script>
+export default {
+    components: {
+        MainLayout: () => import("@/layouts/MainLayout.vue"),
+    },
+}
+</script>
