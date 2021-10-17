bcb2587f503409c5b3c6eaec4ba27dcc4ba0dcc3
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-18 00:43:53 +0800

    update company page content

--- src/pages/Settings/Company.vue
+++ src/pages/Settings/Company.vue
@@ -10,17 +10,146 @@
 </nav>
 <!-- End Breadcrumb -->
 
-<div class="container is-fluid my-5">
-    companies
+<div class="columns">
+  <div class="column is-9">
+    <div class="container is-fluid pr-0">
+      <div class="is-flex is-justify-content-space-between py-2">
+        <b-input 
+          placeholder="Search..."
+          type="search"
+          icon="magnify">
+        </b-input>
+        <b-tooltip label="Add Company">
+          <b-button 
+            icon-right="plus" 
+            class="mr-3" 
+            @click="add" 
+          />
+        </b-tooltip>
+      </div>
+      <b-table
+        :data="isEmpty ? [] : companies"
+        :striped="isStriped"
+        :hoverable="isHoverable"
+        :mobile-cards="hasMobileCards"
+        :paginated="isPaginated"
+        :per-page="perPage"
+        :current-page.sync="currentPage"
+        :pagination-simple="isPaginationSimple"
+        :pagination-position="paginationPosition"
+        :default-sort-direction="defaultSortDirection"
+        :pagination-rounded="isPaginationRounded"
+        :sort-icon="sortIcon"
+        :sort-icon-size="sortIconSize"
+        default-sort="name"
+        aria-next-label="Next page"
+        aria-previous-label="Previous page"
+        aria-page-label="Page"
+        aria-current-label="Current page"
+      >
+        <template v-for="(column, index) in columns">
+          <b-table-column
+            :key="index"
+            :label="column.title"
+            :field="column.field"
+            :visible="column.visible"
+            v-slot="props"
+            sortable
+          >
+            {{ props.row[column.field] }}
+          </b-table-column>
+        </template>
+        <b-table-column field="option" v-slot="props" width="100" centered>
+          <b-button 
+            size="is-small" 
+            type="is-primary" 
+            icon-right="pencil" 
+            @click="edit(props.row)"
+          />
+        </b-table-column>
+        <template #empty>
+          <div class="has-text-centered">No records</div>
+        </template>
+      </b-table>
+    </div>
+  </div>
+  <div class="column is-3">
+
+  </div>
+
+  <!-- Start Modals -->
+  <add-company-modal 
+    :active="addCompanyModal"
+    @close="addCompanyModal = !addCompanyModal"
+  ></add-company-modal>
+
+  <edit-company-modal 
+    :active="editCompanyModal"
+    @close="editCompanyModal = !editCompanyModal"
+  ></edit-company-modal>
+  <!-- End Modals -->
 </div>
 
 </main-layout>
 </template>
 
 <script>
+import {mapActions, mapGetters} from 'vuex';
+
 export default {
-    components: {
-        MainLayout: () => import("@/layouts/MainLayout.vue"),
+  components: {
+    MainLayout: () => import("@/layouts/MainLayout.vue"),
+    AddCompanyModal: () => import("@/components/Settings/Company/AddModal.vue"),
+    EditCompanyModal: () => import("@/components/Settings/Company/EditModal.vue"),
+  },
+
+  data() {
+    return {
+      layout: 'table',
+      columns: [
+        { title: 'Name', field: 'name', visible: true },
+        { title: 'Description', field: 'description', visible: true },
+      ],
+      isEmpty: false,
+      isStriped: false,
+      isHoverable: true,
+      hasMobileCards: true,
+      isPaginated: false,  
+      isPaginationSimple: true,
+      isPaginationRounded: false,
+      paginationPosition: 'bottom',
+      defaultSortDirection: 'asc',
+      sortIcon: 'arrow-up',
+      sortIconSize: 'is-small',
+      currentPage: 1,
+      perPage: 10,
+      addCompanyModal: false,
+      editCompanyModal: false,
+    }
+  },
+
+  computed: {
+    ...mapGetters({
+      companies: 'company/list'
+    })
+  },
+
+  methods: {
+    ...mapActions({
+      setForm: 'company/setForm'
+    }),
+    add() {
+      const company = {
+        name: '',
+        description: ''
+      }
+      this.setForm(company);
+      this.addCompanyModal = true;
     },
+    edit(company) {
+      this.setForm(company);
+      this.editCompanyModal = true;
+    }
+  }
 }
 </script>
