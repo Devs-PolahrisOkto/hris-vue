1dd91a92cef3cc5436f6e47272d7ac97a4127c9b
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-18 08:46:11 +0800

    add AddModal and EditModal for employment type page

---
+++ src/components/Settings/EmploymentType/EditModal.vue
@@ -0,0 +1,45 @@
+<template>
+<b-modal 
+    v-model="active" 
+    :width="640"
+    :can-cancel="['x']"
+>
+    <modal-form 
+        :modal-form="employmentTypeForm"
+        @submit="update"
+        @close="$emit('close')"
+    >
+        <template v-slot:header-title>
+            Edit Employment Type
+        </template>
+    </modal-form>
+</b-modal>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+
+export default {
+    components: {
+        ModalForm: () => import("@/components/Settings/Common/Form.vue"),
+    },
+
+    props: {
+        active: {
+            type: Boolean
+        },
+    },
+
+    computed: {
+        ...mapGetters({
+            employmentTypeForm: 'employmentType/employmentTypeForm'
+        })
+    },
+
+    methods: {
+        update(form) {
+            console.log('update', form)
+        }
+    }
+}
+</script>
