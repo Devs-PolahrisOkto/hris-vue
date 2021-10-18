4a01f12354f44eb60daeebcd922ed2cfccc944b7
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-18 09:06:05 +0800

    add AddModal and EditModal for position page

---
+++ src/components/Settings/Position/EditModal.vue
@@ -0,0 +1,45 @@
+<template>
+<b-modal 
+    v-model="active" 
+    :width="640"
+    :can-cancel="['x']"
+>
+    <modal-form 
+        :modal-form="positionForm"
+        @submit="update"
+        @close="$emit('close')"
+    >
+        <template v-slot:header-title>
+            Edit Position
+        </template>
+    </modal-form>
+</b-modal>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+
+export default {
+    components: {
+        ModalForm: () => import("@/components/Settings/Common/Form.vue"),
+    },
+
+    props: {
+        active: {
+            type: Boolean
+        },
+    },
+
+    computed: {
+        ...mapGetters({
+            positionForm: 'position/positionForm'
+        })
+    },
+
+    methods: {
+        update(form) {
+            console.log('update', form)
+        }
+    }
+}
+</script>
