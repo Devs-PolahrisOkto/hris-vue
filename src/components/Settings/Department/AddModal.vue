d86f8936fe35040a753ada46f1f395b0e8535128
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-21 08:57:02 +0800

    add AddModal for department page

---
+++ src/components/Settings/Department/AddModal.vue
@@ -0,0 +1,45 @@
+<template>
+<b-modal 
+    v-model="active" 
+    :width="640"
+    :can-cancel="['x']"
+>
+    <modal-form 
+        :modal-form="departmentForm"
+        @submit="save"
+        @close="$emit('close')"
+    >
+        <template v-slot:header-title>
+            Add Department
+        </template>
+    </modal-form>
+</b-modal>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+
+export default {
+    components: {
+        ModalForm: () => import("@/components/Settings/Common/Form.vue"),
+    },
+
+    props: {
+        active: {
+            type: Boolean
+        },
+    },
+
+    computed: {
+        ...mapGetters({
+            departmentForm: 'department/departmentForm'
+        })
+    },
+
+    methods: {
+        save(form) {
+            console.log('save', form);
+        }
+    }
+}
+</script>
