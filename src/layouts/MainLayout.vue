314908c5be42ca669f678cc76304689ac68be6d6
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-24 22:19:33 +0800

    refactor main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,35 +1,38 @@
 <template>
-<div>
-    <top-nav
+  <div>
+    <side-nav
+      :toggle-state="toggleState"
+    ></side-nav>
+
+    <div class="main-section">
+      <top-nav
         :toggle-state="toggleState"
         @toggle-sidenav="toggleSideNav"
-    ></top-nav>
-    <side-nav
-        :toggle-state="toggleState"
-    ></side-nav>
-    <div class="main-content" :class="{'show': toggleState}">
+      ></top-nav>
+      <div class="main-section__content">
         <slot></slot>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    components: {
-        TopNav: () => import("@/partials/TopNav.vue"),
-        SideNav: () => import("@/partials/SideNav.vue"),
-    },
+  components: {
+    TopNav: () => import("@/partials/TopNav.vue"),
+    SideNav: () => import("@/partials/SideNav.vue"),
+  },
 
-    data() {
-        return {
-            toggleState: false,
-        }
-    },
-
-    methods: {
-        toggleSideNav() {
-            this.toggleState = !this.toggleState;      
-        }
+  data() {
+    return {
+      toggleState: false,
     }
+  },
+
+  methods: {
+    toggleSideNav() {
+      this.toggleState = !this.toggleState;      
+    }
+  }
 }
 </script>
