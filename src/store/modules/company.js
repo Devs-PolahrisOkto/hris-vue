78e0157f1f0acb783209d360a6bc2ce4766a0903
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-25 00:40:47 +0800

    add company store actions and mutations for company crud

--- src/store/modules/company.js
+++ src/store/modules/company.js
@@ -1,35 +1,70 @@
+import CompanyClient from '@/api/clients/company-client';
+import Vue from 'vue';
+
+const client = new CompanyClient('https://apistaging.polahrisokto.com/api');
+
 const state = {
-    companies: [
-        { 'id': 1, 'name': 'Company A', 'description': '' },
-        { 'id': 2, 'name': 'Company B', 'description': '' },
-        { 'id': 3, 'name': 'Company C', 'description': '' },
-    ],
-    form: {
-        id: '',
-        name: '',
-        description: ''
-    },
+  companies: [],
+  form: {
+    id: '',
+    name: '',
+    description: ''
+  },
 };
 
 const getters = {
-    companyForm(state) {
-        return state.form;
-    },
-    list(state) {
-        return state.companies;
-    }
+  companyForm(state) {
+    return state.form;
+  },
+  list(state) {
+    return state.companies;
+  }
 }
 
 const mutations = {
-    SET_FORM(state, company) {
-        state.form = {...company};
-    },
+  SET_FORM(state, company) {
+    state.form = {...company};
+  },
+  SET_LIST(state, companies) {
+    state.companies = companies;
+  },
+  ADD_COMPANY(state, company) {
+    state.companies.push(company);
+  },
+  UPDATE_COMPANY(state, company) {
+    const index = state.companies.findIndex(obj => { return obj.id === company.id });
+    Vue.set(state.companies, index, company);
+  }
 };
 
 const actions = {
-    setForm({commit}, payload) {
-        commit('SET_FORM', payload);
+  setForm({commit}, payload) {
+    commit('SET_FORM', payload);
+  },
+  async list({commit}) {
+    const {status, data: {data}} = await client.list();
+    if(status !== 200) {
+      console.error('fetching company list failed');
+    } else {
+      commit('SET_LIST', data);
     }
+  },
+  async save({commit}, payload) {
+    const {status, data: {data}} = await client.save(payload);
+    if(status !== 200) {
+      console.error('saving company failed');
+    } else {
+      commit('ADD_COMPANY', data);
+    }
+  },
+  async update({commit}, payload) {
+    const {status, data: {data}} = await client.update(payload);
+    if(status !== 200) {
+      console.error('updating company failed');
+    } else {
+      commit('UPDATE_COMPANY', data);
+    }
+  },
 };
 
 export default {
