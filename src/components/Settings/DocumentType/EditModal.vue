32039116ce26954826eca007ba6679547b5c5ad5
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-27 01:02:36 +0800

    add AddModal and EditModal for adding and editing of document type

---
+++ src/components/Settings/DocumentType/EditModal.vue
@@ -0,0 +1,48 @@
+<template>
+  <b-modal 
+    v-model="active" 
+    :width="640"
+    :can-cancel="['x']"
+  >
+    <modal-form 
+      :modal-form="documentTypeForm"
+      @submit="update"
+      @close="$emit('close')"
+    >
+      <template v-slot:header-title>
+        Edit Document Type
+      </template>
+    </modal-form>
+  </b-modal>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  components: {
+    ModalForm: () => import("@/components/Settings/Common/Form.vue"),
+  },
+
+  props: {
+    active: {
+      type: Boolean
+    },
+  },
+
+  computed: {
+    ...mapGetters({
+      documentTypeForm: 'documentType/documentTypeForm'
+    })
+  },
+
+  methods: {
+    ...mapActions({
+      updateDocumentType: 'documentType/update',
+    }),
+    update(form) {
+      this.updateDocumentType(form).then(this.$emit('close'));
+    }
+  }
+}
+</script>
