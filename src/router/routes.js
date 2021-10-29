1a29bbe646fd58dd2959422625ecd8a9578f3f2f
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-30 01:54:27 +0800

    add meta parent for routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -27,73 +27,109 @@ const routes = [
     path: '/employees', 
     name: 'AllEmployees', 
     component: () => import(/* webpackChunkName: "AllEmployees" */ '@/pages/Employees/AllEmployees.vue'),
-    meta: { forAuth: true }
+    meta: { 
+      forAuth: true,
+      parent: 'employees'
+    }
   },
   { 
     path: '/employees/register', 
     name: 'RegisterEmployee', 
     component: () => import(/* webpackChunkName: "RegisterEmployee" */ '@/pages/Employees/RegisterEmployee.vue'),
-    meta: { forAuth: true }
+    meta: { 
+      forAuth: true,
+      parent: 'employees'
+    }
   },
   { 
     path: '/employees/import', 
     name: 'ImportEmployees', 
     component: () => import(/* webpackChunkName: "ImportEmployees" */ '@/pages/Employees/ImportEmployees.vue'),
-    meta: { forAuth: true }
+    meta: { 
+      forAuth: true,
+      parent: 'employees'
+    }
   },
   { 
     path: '/employees/:id', 
     name: 'EmployeeProfile', 
     component: () => import(/* webpackChunkName: "EmployeeProfile" */ '@/pages/Employee/Employee.vue'),
-    meta: { forAuth: true }
+    meta: { 
+      forAuth: true,
+      parent: 'employees'
+    }
   },
   { 
-    path: '/companies', 
+    path: '/settings/companies', 
     name: 'Company', 
     component: () => import(/* webpackChunkName: "EmployeeProfile" */ '@/pages/Settings/Company.vue'),
-    meta: { forAuth: true }
+    meta: { 
+      forAuth: true,
+      parent: 'settings'
+    }
   },
   { 
-    path: '/employment-types', 
+    path: '/settings/employment-types', 
     name: 'EmploymentType', 
     component: () => import(/* webpackChunkName: "EmploymentType" */ '@/pages/Settings/EmploymentType.vue'),
-    meta: { forAuth: true }
+    meta: { 
+      forAuth: true,
+      parent: 'settings'
+    }
   },
   { 
-    path: '/positions', 
+    path: '/settings/positions', 
     name: 'Position', 
     component: () => import(/* webpackChunkName: "Position" */ '@/pages/Settings/Position.vue'),
-    meta: { forAuth: true }
+    meta: { 
+      forAuth: true,
+      parent: 'settings'
+    }
   },
   { 
-    path: '/departments', 
+    path: '/settings/departments', 
     name: 'Department', 
     component: () => import(/* webpackChunkName: "Department" */ '@/pages/Settings/Department.vue'),
-    meta: { forAuth: true }
+    meta: { 
+      forAuth: true,
+      parent: 'settings'
+    }
   },
   { 
-    path: '/address-types', 
+    path: '/settings/address-types', 
     name: 'AddressType', 
     component: () => import(/* webpackChunkName: "AddressType" */ '@/pages/Settings/AddressType.vue'),
-    meta: { forAuth: true }
+    meta: { 
+      forAuth: true,
+      parent: 'settings'
+    }
   },
   { 
-    path: '/document-types', 
+    path: '/settings/document-types', 
     name: 'DocumentType', 
     component: () => import(/* webpackChunkName: "DocumentType" */ '@/pages/Settings/DocumentType.vue'),
-    meta: { forAuth: true }
+    meta: { 
+      forAuth: true,
+      parent: 'settings'
+    }
   },
   { 
-    path: '/banks', 
+    path: '/settings/banks', 
     name: 'Bank', 
     component: () => import(/* webpackChunkName: "Bank" */ '@/pages/Settings/Bank.vue'),
-    meta: { forAuth: true }
+    meta: { 
+      forAuth: true,
+      parent: 'settings'
+    }
   },
   { 
-    path: '/civil-statuses', 
+    path: '/settings/civil-statuses', 
     name: 'CivilStatus', 
     component: () => import(/* webpackChunkName: "CivilStatus" */ '@/pages/Settings/CivilStatus.vue'),
-    meta: { forAuth: true }
+    meta: { 
+      forAuth: true,
+      parent: 'settings'
+    }
   },
 ];
 
