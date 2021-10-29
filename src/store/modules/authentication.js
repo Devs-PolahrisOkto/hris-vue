525b86a7a7b096a92ae4971b1d98bbaed9a49d8d
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-30 02:17:38 +0800

    add authUser getters

--- src/store/modules/authentication.js
+++ src/store/modules/authentication.js
@@ -3,47 +3,63 @@ import AuthenticationClient from '@/api/clients/authentication-client';
 const client = new AuthenticationClient('https://apistaging.polahrisokto.com/api');
 
 const state = {
-    authToken: localStorage.getItem('authToken'),
-    authUser: JSON.parse(localStorage.getItem('authUser')),
+  authToken: localStorage.getItem('authToken'),
+  authUser: JSON.parse(localStorage.getItem('authUser')),
 };
 
+const getters = {
+  authUserObj(state) {
+    return state.authUser;
+  },
+  authUserName(state) {
+    return state.authUser ? `${state.authUser.firstname} ${state.authUser.lastname}` : '';
+  },
+  authUserAvatar(state) {
+    return state.authUser && state.authUser.avatar;
+  },
+  authUserEmail(state) {
+    return state.authUser && state.authUser.email;
+  }
+}
+
 const mutations = {
-    SET_AUTH_TOKEN (state, token) {
-        state.authToken = token;
-        localStorage.setItem('authToken', token);
-    },
-    SET_AUTH_USER (state, user) {
-        state.authUser = user;
-        localStorage.setItem('authUser', JSON.stringify(user));
-    },
-    CLEAR_AUTH_USER (state) {
-        state.authUser = null;
-        state.authToken = null;
-        localStorage.removeItem('authToken');
-        localStorage.removeItem('authUser');
-    }
+  SET_AUTH_TOKEN (state, token) {
+    state.authToken = token;
+    localStorage.setItem('authToken', token);
+  },
+  SET_AUTH_USER (state, user) {
+    state.authUser = user;
+    localStorage.setItem('authUser', JSON.stringify(user));
+  },
+  CLEAR_AUTH_USER (state) {
+    state.authUser = null;
+    state.authToken = null;
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('authUser');
+  }
 };
 
 const actions = {
-    async login({commit}, payload) {
-        const {status, data: {token, user}} = await client.login(payload);
-        if(status !== 200) {
-            console.error('login failed');
-            return false;
-        } else {
-            commit('SET_AUTH_TOKEN', token);
-            commit('SET_AUTH_USER', user);
-            return true;
-        }
-    },
-    logout({commit}) {
-        commit('CLEAR_AUTH_USER');
+  async login({commit}, payload) {
+    const {status, data: {token, user}} = await client.login(payload);
+    if(status !== 200) {
+      console.error('login failed');
+      return false;
+    } else {
+      commit('SET_AUTH_TOKEN', token);
+      commit('SET_AUTH_USER', user);
+      return true;
     }
+  },
+  logout({commit}) {
+    commit('CLEAR_AUTH_USER');
+  }
 };
 
 export default {
-    namespaced: true,
-    state, 
-    mutations, 
-    actions,
+  namespaced: true,
+  state,
+  getters,
+  mutations, 
+  actions,
 };
