a7a4f3a9e14f021e0d0ef7c449935add07210d99
wickeddd3 <pablophilipandrew@gmail.com>
2021-10-31 15:49:59 +0800

    use navigation store toggleSidebarMini action when toggling sidebar mini

--- src/partials/TopNav.vue
+++ src/partials/TopNav.vue
@@ -3,26 +3,28 @@
     class="topbar" 
     id="header"
   >
-    <div class="topbar__toggle" @click="toggleSideNav">
-      <i id="topbar-toggle" class="mdi mdi-menu-open" v-show="!toggleState"></i>
-      <i id="topbar-toggle" class="mdi mdi-backburger" v-show="toggleState"></i>
+    <div class="topbar__toggle" @click="toggleSidebarMini">
+      <i id="topbar-toggle" class="mdi mdi-menu-open" v-show="!sidebarMiniState"></i>
+      <i id="topbar-toggle" class="mdi mdi-backburger" v-show="sidebarMiniState"></i>
     </div>
     <div class="topbar__menu"></div>
   </header>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+
 export default {
-  props: {
-    toggleState: {
-      type: Boolean
-    }
+  computed: {
+    ...mapGetters({
+      sidebarMiniState: 'navigation/sidebarMiniState',
+    }),
   },
 
   methods: {
-    toggleSideNav() {
-      this.$emit('toggle-sidenav');
-    },
+    ...mapActions({
+      toggleSidebarMini: 'navigation/toggleSidebarMini',
+    }),
   }
 }
 </script>
