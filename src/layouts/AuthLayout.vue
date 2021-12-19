2cffa68cdf9dc1334eb6cb294f803c3ee8cebac8
wickeddd3 <pablophilipandrew@gmail.com>
2021-12-19 14:30:54 +0800

    refactor spacing

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,9 +1,9 @@
 <template>
-<div class="container mt-6">
+  <div class="container mt-6">
     <div class="columns is-flex flex-wrap-wrap is-justify-content-center">
-        <div class="column is-5">
-            <slot></slot>
-        </div>
+      <div class="column is-5">
+        <slot></slot>
+      </div>
     </div>
-</div>
+  </div>
 </template>
