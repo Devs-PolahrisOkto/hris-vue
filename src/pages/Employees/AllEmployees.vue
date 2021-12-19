681a90ae2d40938fa2732c8f0cb04e83e60e826b
wickeddd3 <pablophilipandrew@gmail.com>
2021-12-19 14:51:22 +0800

    add card on employees table

--- src/pages/Employees/AllEmployees.vue
+++ src/pages/Employees/AllEmployees.vue
@@ -11,127 +11,131 @@
 
     <!-- Start Active Employees -->
     <div class="container is-fluid my-5">
-      <div class="is-flex is-justify-content-space-between py-2">
-        <b-input
-          v-model="searchField"
-          placeholder="Search..."
-          type="search"
-          icon="magnify">
-        </b-input>
-        <div>
-          <b-tooltip label="Filter">
-            <b-button
-              icon-right="filter"
-              class="mr-1"
-              @click="isFilterModalActive = !isFilterModalActive"
-            />
-          </b-tooltip>
-          <b-tooltip label="Layout">
-            <b-button
-              icon-right="grid-large"
-              class="mr-1"
-              @click="isLayoutModalActive = !isLayoutModalActive"
-            />
-          </b-tooltip>
-          <b-tooltip label="Columns">
-            <b-button
-              icon-right="table-column"
-              @click="isColumnModalActive = !isColumnModalActive"
-            />
-          </b-tooltip>
+      <div class="card p-2">
+        <!-- Header Options -->
+        <div class="is-flex is-justify-content-space-between py-2">
+          <b-input
+            v-model="searchField"
+            placeholder="Search..."
+            type="search"
+            icon="magnify">
+          </b-input>
+          <div>
+            <b-tooltip label="Filter">
+              <b-button
+                icon-right="filter"
+                class="mr-1"
+                @click="isFilterModalActive = !isFilterModalActive"
+              />
+            </b-tooltip>
+            <b-tooltip label="Layout">
+              <b-button
+                icon-right="grid-large"
+                class="mr-1"
+                @click="isLayoutModalActive = !isLayoutModalActive"
+              />
+            </b-tooltip>
+            <b-tooltip label="Columns">
+              <b-button
+                icon-right="table-column"
+                @click="isColumnModalActive = !isColumnModalActive"
+              />
+            </b-tooltip>
+          </div>
         </div>
-      </div>
+        <!-- Header Options -->
 
-      <!-- Table Layout -->
-      <div v-show="layout === 'table'">
-        <b-table
-          :data="isEmpty ? [] : filteredEmployees"
-          :striped="isStriped"
-          :hoverable="isHoverable"
-          :mobile-cards="hasMobileCards"
-          :paginated="isPaginated"
-          :per-page="perPage"
-          :current-page.sync="currentPage"
-          :pagination-simple="isPaginationSimple"
-          :pagination-position="paginationPosition"
-          :default-sort-direction="defaultSortDirection"
-          :pagination-rounded="isPaginationRounded"
-          :sort-icon="sortIcon"
-          :sort-icon-size="sortIconSize"
-          default-sort="name"
-          aria-next-label="Next page"
-          aria-previous-label="Previous page"
-          aria-page-label="Page"
-          aria-current-label="Current page"
-        >
-          <b-table-column v-slot="props" field="avatar" width="40">
-            <figure class="image is-24x24">
-              <img class="is-rounded" :src="props.row.avatar">
-            </figure>
-          </b-table-column>
-
-          <template v-for="(column, index) in columns">
-            <b-table-column
-              :key="index"
-              v-slot="props"
-              :label="column.title"
-              :field="column.field"
-              :visible="column.visible"
-              sortable
-            >
-              {{ props.row[column.field] }}
+        <!-- Table Layout -->
+        <div v-show="layout === 'table'">
+          <b-table
+            :data="isEmpty ? [] : filteredEmployees"
+            :striped="isStriped"
+            :hoverable="isHoverable"
+            :mobile-cards="hasMobileCards"
+            :paginated="isPaginated"
+            :per-page="perPage"
+            :current-page.sync="currentPage"
+            :pagination-simple="isPaginationSimple"
+            :pagination-position="paginationPosition"
+            :default-sort-direction="defaultSortDirection"
+            :pagination-rounded="isPaginationRounded"
+            :sort-icon="sortIcon"
+            :sort-icon-size="sortIconSize"
+            default-sort="name"
+            aria-next-label="Next page"
+            aria-previous-label="Previous page"
+            aria-page-label="Page"
+            aria-current-label="Current page"
+          >
+            <b-table-column v-slot="props" field="avatar" width="40">
+              <figure class="image is-24x24">
+                <img class="is-rounded" :src="props.row.avatar">
+              </figure>
             </b-table-column>
-          </template>
 
-          <b-table-column v-slot="props" field="option">
-            <b-button
-              size="is-small"
-              type="is-primary"
-              icon-right="eye"
-              tag="router-link"
-              :to="`/employees/${props.row.id}`"
-            />
-          </b-table-column>
+            <template v-for="(column, index) in columns">
+              <b-table-column
+                :key="index"
+                v-slot="props"
+                :label="column.title"
+                :field="column.field"
+                :visible="column.visible"
+                sortable
+              >
+                {{ props.row[column.field] }}
+              </b-table-column>
+            </template>
 
-          <template #empty>
-            <div class="has-text-centered">No records</div>
-          </template>
-        </b-table>
-      </div>
-      <!-- Table Layout -->
+            <b-table-column v-slot="props" field="option">
+              <b-button
+                size="is-small"
+                type="is-primary"
+                icon-right="eye"
+                tag="router-link"
+                :to="`/employees/${props.row.id}`"
+              />
+            </b-table-column>
 
-      <!-- Grid Layout -->
-      <div v-show="layout === 'grid'">
-        <div class="columns is-flex is-flex-wrap-wrap">
-          <template v-for="(item, index) in employees">
-            <div :key="index" class="column is-6-desktop is-12-tablet is-12-mobile">
-              <article class="media has-background-white p-4">
-                <figure class="media-left">
-                  <p class="image is-128x128">
-                    <img :src="item.avatar">
-                  </p>
-                </figure>
-                <div class="media-content">
-                  <h5 class="is-size-5 has-text-weight-semibold">{{ item.employeeName }}</h5>
-                  <h6 class="is-size-6 has-text-grey">{{ item.position }}</h6>
-                  <h6 class="is-size-6">{{ item.employment_type }}</h6>
-                  <h6 class="is-size-6">{{ item.department }}</h6>
-                </div>
-                <div class="media-right">
-                  <b-button
-                    size="is-small"
-                    type="is-primary"
-                    icon-right="eye"
-                    tag="router-link"
-                    :to="`/employees/${item.id}`"
-                  />
-                </div>
-              </article>
-            </div>
-          </template>
+            <template #empty>
+              <div class="has-text-centered">No records</div>
+            </template>
+          </b-table>
         </div>
+        <!-- Table Layout -->
+
+        <!-- Grid Layout -->
+        <div v-show="layout === 'grid'">
+          <div class="columns is-flex is-flex-wrap-wrap">
+            <template v-for="(item, index) in employees">
+              <div :key="index" class="column is-6-desktop is-12-tablet is-12-mobile">
+                <article class="media has-background-white p-4">
+                  <figure class="media-left">
+                    <p class="image is-128x128">
+                      <img :src="item.avatar">
+                    </p>
+                  </figure>
+                  <div class="media-content">
+                    <h5 class="is-size-5 has-text-weight-semibold">{{ item.employeeName }}</h5>
+                    <h6 class="is-size-6 has-text-grey">{{ item.position }}</h6>
+                    <h6 class="is-size-6">{{ item.employment_type }}</h6>
+                    <h6 class="is-size-6">{{ item.department }}</h6>
+                  </div>
+                  <div class="media-right">
+                    <b-button
+                      size="is-small"
+                      type="is-primary"
+                      icon-right="eye"
+                      tag="router-link"
+                      :to="`/employees/${item.id}`"
+                    />
+                  </div>
+                </article>
+              </div>
+            </template>
+          </div>
+        </div>
+        <!-- Grid Layout -->
       </div>
-      <!-- Grid Layout -->
     </div>
     <!-- End Active Employees -->
 
