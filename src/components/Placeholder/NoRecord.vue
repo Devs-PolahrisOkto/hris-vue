3f9852aaf27cfdf36c92815ddd4ada055fb6fe35
wickeddd3 <pablophilipandrew@gmail.com>
2021-12-19 15:20:36 +0800

    add no record placeholder component

---
+++ src/components/Placeholder/NoRecord.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="is-flex is-justify-content-center is-align-items-center is-flex-direction-column p-5">
+    <no-record-illustration></no-record-illustration>
+    <div class="has-text-weight-light p-5">No records.</div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    NoRecordIllustration: () => import('@/components/Illustrations/NoRecordIllustration'),
+  },
+};
+</script>
