b6681aad5ff376cef2eb32e7cd52eb57fe004c67
Superern <ernandrewgregorio@gmail.com>
2021-12-20 00:13:09 +0800

    autoformat line space

--- vue.config.js
+++ vue.config.js
@@ -14,12 +14,12 @@ module.exports = {
       title: 'Index Page',
       // chunks to include on this page, by default includes
       // extracted common chunks and vendor chunks.
-      chunks: [ 'chunk-vendors', 'chunk-common', 'index' ],
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
     },
     // when using the entry-only string format,
     // template is inferred to be `public/subpage.html`
     // and falls back to `public/index.html` if not found.
     // Output filename is inferred to be `subpage.html`.
     // subpage: 'src/subpage/main.js'
-  },
-};
+  }
+}
