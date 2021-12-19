7c74cd71de451cbf89fa335144da3cbe507f77cc
wickeddd3 <pablophilipandrew@gmail.com>
2021-12-19 14:31:13 +0800

    add setting layout

---
+++ src/layouts/SettingLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="columns">
+    <div class="column is-9-desktop is-12-tablet is-12-mobile">
+      <div class="container is-fluid">
+        <div class="card p-2">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+    <div class="column is-3"></div>
+  </div>
+</template>
