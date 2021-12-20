c6724058a50aa27a7e9171135a209177665769eb
wickeddd3 <pablophilipandrew@gmail.com>
2021-12-21 01:02:54 +0800

    add props on norecord component placeholder

--- src/components/Placeholder/NoRecord.vue
+++ src/components/Placeholder/NoRecord.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="is-flex is-justify-content-center is-align-items-center is-flex-direction-column p-5">
-    <no-record-illustration></no-record-illustration>
+    <no-record-illustration :width="width" :height="height"></no-record-illustration>
     <div class="has-text-weight-light p-5">No records.</div>
   </div>
 </template>
@@ -9,6 +9,17 @@
 export default {
   components: {
     NoRecordIllustration: () => import('@/components/Illustrations/NoRecordIllustration'),
+  },
+
+  props: {
+    width: {
+      type: Number,
+      default: 250.71039,
+    },
+    height: {
+      type: Number,
+      default: 267.98584,
+    },
   },
 };
 </script>
