361dceaf325aa20d949d117ca0c756fe6e62c625
wickeddd3 <pablophilipandrew@gmail.com>
2021-12-20 23:56:40 +0800

    remove selectable functionality on table and refactor spacing

--- src/pages/Employees/ImportEmployees.vue
+++ src/pages/Employees/ImportEmployees.vue
@@ -1,73 +1,65 @@
 <template>
-<main-layout>
-
+  <main-layout>
     <!-- Start Breadcrumb -->
     <nav class="breadcrumb px-5" aria-label="breadcrumbs">
-        <ul class="px-3 pt-3">
-            <li><a href="#">Admin</a></li>
-            <li><a href="#">Employees</a></li>
-            <li class="is-active"><a href="#" aria-current="page">Import Employees</a></li>
-        </ul>
+      <ul class="px-3 pt-3">
+        <li><a href="#">Admin</a></li>
+        <li><a href="#">Employees</a></li>
+        <li class="is-active"><a href="#" aria-current="page">Import Employees</a></li>
+      </ul>
     </nav>
     <!-- End Breadcrumb -->
 
     <!-- Import Employees -->
     <div class="container is-fluid my-5">
+      <div class="card p-2">
         <div
-          class="is-flex is-justify-content-space-between
+          class="is-flex is-justify-content-flex-end
           is-align-items-center is-flex-wrap-wrap"
         >
-            <div class="pb-2">
-                <b-button
-                    icon-right="delete-forever"
-                    type="is-danger"
-                    :disabled="selectedData.length === 0"
-                    class="mr-1"
-                    @click="removeSelectedData" />
-            </div>
-            <div class="is-flex is-flex-wrap-wrap">
-                <b-field class="file mr-3">
-                    <span v-if="file" class="is-size-6 px-3 py-2">{{ file }}</span>
-                    <b-upload>
-                        <a class="button">
-                            <b-icon icon="file-import" size="is-small"></b-icon>
-                            <span class="is-size-6 has-text-weight-light">Import Excel File</span>
-                        </a>
-                        <input ref="fileInput" type="file" @change="change">
-                    </b-upload>
-                </b-field>
-                <b-tooltip label="Upload">
-                    <b-button
-                        icon-right="cloud-upload"
-                        class="mr-1" />
-                </b-tooltip>
-                <b-tooltip label="Template">
-                    <b-button
-                        icon-right="file-download"
-                        @click="downloadTemplate()" />
-                </b-tooltip>
-            </div>
+          <div class="is-flex is-flex-wrap-wrap">
+            <b-field class="file mr-3">
+              <span v-if="file" class="is-size-6 px-3 py-2">{{ file }}</span>
+              <b-upload>
+                <a class="button">
+                  <b-icon icon="file-import" size="is-small"></b-icon>
+                  <span class="is-size-6 has-text-weight-light">Import Excel File</span>
+                </a>
+                <input ref="fileInput" type="file" @change="change">
+              </b-upload>
+            </b-field>
+            <b-tooltip label="Upload">
+              <b-button
+                icon-right="cloud-upload"
+                class="mr-1"
+              />
+            </b-tooltip>
+            <b-tooltip label="Template">
+              <b-button
+                icon-right="file-download"
+                @click="downloadTemplate()"
+              />
+            </b-tooltip>
+          </div>
         </div>
-
+        <!-- Imported Data Table -->
         <b-table
-            :data="importedData"
-            :columns="columns"
-            :sticky-header="stickyHeaders"
-            :checked-rows.sync="selectedData"
-            checkable
-            sticky-checkbox
-            striped
-            narrowed
-            hoverable
+          :data="importedData"
+          :columns="columns"
+          :sticky-header="stickyHeaders"
+          striped
+          narrowed
+          hoverable
         >
-            <template #empty>
-                <div class="has-text-centered p-4">No Data</div>
-            </template>
+          <template #empty>
+            <no-record></no-record>
+          </template>
         </b-table>
+        <!-- Imported Data Table -->
+      </div>
     </div>
     <!-- Import Employees -->
-
-</main-layout>
+  </main-layout>
 </template>
 
 <script>
@@ -77,70 +69,46 @@ import { saveAs } from 'file-saver';
 export default {
   components: {
     MainLayout: () => import('@/layouts/MainLayout.vue'),
+    NoRecord: () => import('@/components/Placeholder/NoRecord.vue'),
   },
 
   data () {
     return {
       file: null,
       importedData: [],
-      selectedData: [],
       columns: [
         {
-          field: 'FirstName',
+          field: 'firstname',
           label: 'First Name',
         },
         {
-          field: 'LastName',
-          label: 'Last Name',
-        },
-        {
-          field: 'MiddleName',
+          field: 'middlename',
           label: 'Middle Name',
         },
         {
-          field: 'Position',
-          label: 'Position',
+          field: 'lastname',
+          label: 'Last Name',
         },
         {
-          field: 'Department',
-          label: 'Department',
+          field: 'nickname',
+          label: 'Nickname',
         },
         {
-          field: 'EmploymentType',
-          label: 'Employment Type',
+          field: 'title',
+          label: 'Title',
         },
         {
-          field: 'Birthdate',
+          field: 'birthDate',
           label: 'Birthdate',
+        },
+        {
+          field: 'username',
+          label: 'Username',
           centered: true,
         },
         {
-          field: 'Gender',
-          label: 'Gender',
-        },
-        {
-          field: 'CivilStatus',
-          label: 'Civil Status',
-        },
-        {
-          field: 'SSS',
-          label: 'SSS',
-          width: '170px',
-        },
-        {
-          field: 'Philhealth',
-          label: 'PhilHealth',
-          width: '170px',
-        },
-        {
-          field: 'HDMF',
-          label: 'HDMF',
-          width: '170px',
-        },
-        {
-          field: 'TIN',
-          label: 'TIN',
-          width: '170px',
+          field: 'email',
+          label: 'Email',
         },
       ],
       stickyHeaders: true,
@@ -162,7 +130,7 @@ export default {
         this.importedData = worksheetData;
         this.$refs.fileInput.value = '';
       };
-      reader.readAsBinaryString(file);
+      reader.readAsBinaryString(firstFile);
     },
     downloadTemplate () {
       const excelFormat = 'xlsx';
@@ -176,19 +144,14 @@ export default {
       workbook.SheetNames.push('Employee Details');
       const templateHeader = [
         [
-          'FirstName',
-          'LastName',
-          'MiddleName',
-          'Position',
-          'Department',
-          'EmploymentType',
-          'Birthdate',
-          'Gender',
-          'CivilStatus',
-          'SSS',
-          'Philhealth',
-          'HDMF',
-          'TIN',
+          'firstname',
+          'middlename',
+          'lastname',
+          'nickname',
+          'title',
+          'birthDate',
+          'username',
+          'email',
         ],
       ];
       const Sheet1 = XLSX.utils.aoa_to_sheet(templateHeader);
@@ -205,12 +168,6 @@ export default {
       saveAs(new Blob([ s2ab(wbout) ], {
         type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
       }), `Employees Template.${excelFormat}`);
-    },
-    removeSelectedData () {
-      this.selectedData.forEach(item => {
-        this.importedData.splice(this.importedData.indexOf(item), 1);
-      });
-      this.selectedData = [];
     },
   },
 };
