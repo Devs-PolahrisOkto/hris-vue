4aa422788a923561651d13f3be8acc1ad7c0c58d
wickeddd3 <pablophilipandrew@gmail.com>
2021-12-22 00:56:08 +0800

    commented add button

--- src/components/Employee/Personal/Address/Addresses.vue
+++ src/components/Employee/Personal/Address/Addresses.vue
@@ -2,7 +2,7 @@
   <div class="card mb-4">
     <header class="is-flex is-justify-content-space-between is-align-items-center py-2 pr-2">
       <h6 class="is-size-6 has-text-weight-light px-3">Addresses</h6>
-      <b-button size="is-small" icon-right="refresh" />
+      <!-- <b-button size="is-small" icon-right="plus" /> -->
     </header>
     <b-table
       :data="isEmpty ? [] : addresses"
