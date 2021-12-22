ea75e642159676861210dfede3b6fc4b64011a2e
wickeddd3 <pablophilipandrew@gmail.com>
2021-12-22 09:30:15 +0800

    add schedules page with initial layout

---
+++ src/pages/Timekeeping/Schedules.vue
@@ -0,0 +1,22 @@
+<template>
+  <main-layout>
+    <!-- Start Breadcrumb -->
+    <nav class="breadcrumb px-5" aria-label="breadcrumbs">
+      <ul class="px-3 pt-3">
+        <li><a href="#">Admin</a></li>
+        <li class="is-active"><a href="#" aria-current="page">Schedules</a></li>
+      </ul>
+    </nav>
+    <!-- End Breadcrumb -->
+
+    <h1>Schedules</h1>
+  </main-layout>
+</template>
+
+<script>
+export default {
+  components: {
+    MainLayout: () => import('@/layouts/MainLayout.vue'),
+  },
+};
+</script>
