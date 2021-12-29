2e4b2f2af6e0fec6e60de1e6ebd324c6e62adfab
wickeddd3 <pablophilipandrew@gmail.com>
2021-12-29 16:31:18 +0800

    refactor settings common form

--- src/components/Settings/Common/Form.vue
+++ src/components/Settings/Common/Form.vue
@@ -1,42 +1,46 @@
 <template>
-<ValidationObserver ref="observer" v-slot="{ invalid, passes }">
+  <ValidationObserver ref="observer" v-slot="{ invalid, passes }">
     <form @submit.prevent="passes(submit)">
-        <div class="modal-card">
-            <header class="modal-card-head is-flex is-justify-content-space-between">
-                <h6 class="is-size-6 has-text-weight-bold">
-                    <slot name="header-title"></slot>
-                </h6>
-                <button
-                    type="button"
-                    class="delete"
-                    @click="$emit('close')"></button>
-            </header>
-            <section class="modal-card-body">
-                <text-field
-                    v-model="form.name"
-                    label="Name"
-                    rules="required"
-                    mode="eager"
-                ></text-field>
-                <text-field
-                    v-model="form.description"
-                    label="Description"
-                    mode="eager"
-                ></text-field>
-            </section>
-            <footer class="modal-card-foot">
-                <b-button
-                    label="Close"
-                    @click="$emit('close')" />
-                <b-button
-                    :disabled="invalid"
-                    native-type="submit"
-                    label="Save"
-                    type="is-primary" />
-            </footer>
-        </div>
+      <div class="modal-card">
+        <header class="modal-card-head is-flex is-justify-content-space-between">
+          <h6 class="is-size-6 has-text-weight-bold">
+            <slot name="header-title"></slot>
+          </h6>
+          <button
+            type="button"
+            class="delete"
+            @click="$emit('close')"
+          ></button>
+        </header>
+        <section class="modal-card-body">
+          <text-field
+            v-model="form.name"
+            label="Name"
+            rules="required"
+            mode="eager"
+          ></text-field>
+          <text-field
+            v-model="form.description"
+            label="Description"
+            rules="required"
+            mode="eager"
+          ></text-field>
+        </section>
+        <footer class="modal-card-foot is-flex is-justify-content-end">
+          <b-button
+            label="Cancel"
+            @click="$emit('close')"
+          />
+          <b-button
+            :disabled="invalid"
+            native-type="submit"
+            label="Save"
+            type="is-primary"
+          />
+        </footer>
+      </div>
     </form>
-</ValidationObserver>
+  </ValidationObserver>
 </template>
 
 <script>
