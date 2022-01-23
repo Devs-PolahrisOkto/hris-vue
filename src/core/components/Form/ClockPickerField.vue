1394f1d92bca911880d8494d9a67a3c717d36b1b
wickeddd3 <pablophilipandrew@gmail.com>
2022-01-23 22:22:04 +0800

    add clock picker field component

---
+++ src/core/components/Form/ClockPickerField.vue
@@ -0,0 +1,35 @@
+<template>
+  <ValidationProvider
+    v-slot="{ errors, valid }"
+    :name="label"
+    :rules="rules"
+    :mode="mode"
+  >
+    <b-field
+      :label="label"
+      :label-position="labelPosition"
+      :type="{ 'is-danger': errors[0], 'is-success': valid }"
+      :message="errors"
+    >
+      <b-clockpicker
+        :icon="icon"
+        editable
+        :value="value"
+        @input="$emit('input', $event)"
+      ></b-clockpicker>
+    </b-field>
+  </ValidationProvider>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    value: [ Date, String ],
+    labelPosition: String,
+    mode: String,
+    rules: String,
+    icon: String,
+  },
+};
+</script>
