94ef755e1c2576d9278529802d0344265eb765bf
wickeddd3 <pablophilipandrew@gmail.com>
2022-02-12 20:20:01 +0800

    add branch state config

---
+++ src/config/branch.state.js
@@ -0,0 +1,31 @@
+export default {
+  table: {
+    columns: [
+      { title: 'Name', field: 'name', visible: true },
+      { title: 'Description', field: 'description', visible: true },
+    ],
+    list: [],
+    meta: {
+      isEmpty: false,
+      isStriped: false,
+      isHoverable: true,
+      hasMobileCards: true,
+      isPaginated: false,
+      isPaginationSimple: true,
+      isPaginationRounded: false,
+      paginationPosition: 'bottom',
+      defaultSortDirection: 'asc',
+      defaultSortColumn: 'name',
+      sortIcon: 'arrow-up',
+      sortIconSize: 'is-small',
+      currentPage: 1,
+      perPage: 10,
+    },
+  },
+  form: {
+    id: '',
+    name: '',
+    description: '',
+    company_id: '',
+  },
+};
