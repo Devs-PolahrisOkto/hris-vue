ea30ab09849b1f0df3bb58de1a50c13935de1408
wickeddd3 <pablophilipandrew@gmail.com>
2022-02-27 22:24:39 +0800

    add experience representation

---
+++ src/api/representations/ExperienceRepresentation.js
@@ -0,0 +1,59 @@
+import Representation from '@/api/representations/Representation';
+import { formatDateToString, formatDateTime } from '@/filters/date';
+
+class ExperienceRepresentation extends Representation {
+  constructor (properties) {
+    super({
+      id: null,
+      company: '',
+      project_name: '',
+      description: '',
+      role: '',
+      salary: '',
+      start_at: null,
+      end_at: null,
+      ...properties,
+    });
+  }
+
+  // eslint-disable-next-line camelcase
+  get start_at () {
+    return this._start_at;
+  }
+
+  // eslint-disable-next-line camelcase
+  set start_at (value) {
+    this._start_at = value ? new Date(value) : null;
+  }
+
+  // eslint-disable-next-line camelcase
+  get end_at () {
+    return this._end_at;
+  }
+
+  // eslint-disable-next-line camelcase
+  set end_at (value) {
+    this._end_at = value ? new Date(value) : null;
+  }
+
+  get asViewData () {
+    const transformedData = {
+      ...this,
+      inclusiveDates: `(${formatDateToString(this.start_at)} - ${formatDateToString(this.end_at)})`,
+    };
+    return transformedData;
+  }
+
+  get asPayload () {
+    const transformedData = {
+      ...this,
+      start_at: formatDateTime(this._start_at),
+      end_at: formatDateTime(this._end_at),
+    };
+    delete transformedData._start_at;
+    delete transformedData._end_at;
+    return transformedData;
+  }
+}
+
+export default ExperienceRepresentation;
