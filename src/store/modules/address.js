316c48f3df70700f650c43bbb5126e3ea3ddb225
wickeddd3 <pablophilipandrew@gmail.com>
2022-03-01 23:34:24 +0800

    add current selected employee id to api request

--- src/store/modules/address.js
+++ src/store/modules/address.js
@@ -16,16 +16,18 @@ const mutations = {
 };
 
 const actions = {
-  async save ({ dispatch }, payload) {
-    const { status, data: { data } } = await client.save(payload);
+  async save ({ dispatch, rootGetters }, payload) {
+    const userId = rootGetters['employee/selected']?.user?.id;
+    const { status, data: { data } } = await client.save(userId, payload);
     if (status !== 200) {
       console.error('saving address failed');
     } else {
       dispatch('employee/address/add', data, { root: true });
     }
   },
-  async update ({ dispatch }, payload) {
-    const { status, data: { data } } = await client.update(payload);
+  async update ({ dispatch, rootGetters }, payload) {
+    const userId = rootGetters['employee/selected']?.user?.id;
+    const { status, data: { data } } = await client.update(userId, payload);
     if (status !== 200) {
       console.error('updating address failed');
     } else {
