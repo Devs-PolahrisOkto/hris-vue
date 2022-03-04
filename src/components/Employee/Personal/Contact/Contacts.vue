43ec1410c0b94c430121bbeb2f0e001cfcc18c62
wickeddd3 <pablophilipandrew@gmail.com>
2022-03-04 20:56:35 +0800

    add contact add and edit functionality

--- src/components/Employee/Personal/Contact/Contacts.vue
+++ src/components/Employee/Personal/Contact/Contacts.vue
@@ -2,7 +2,13 @@
   <div class="card mb-4">
     <header class="is-flex is-justify-content-space-between is-align-items-center py-2 pr-2">
       <h6 class="is-size-6 has-text-weight-light px-3">Contacts</h6>
+      <b-button
+        size="is-small"
+        icon-right="plus"
+        @click="add"
+      />
     </header>
+    <!-- Contacts -->
     <template v-for="item in contacts">
       <div
         :key="item.id"
@@ -10,8 +16,27 @@
       >
         <h6 class="is-size-6 has-text-weight-medium">{{ item.name }}</h6>
         <h6 class="is-size-6">{{ item.description }}</h6>
+        <b-button
+          size="is-small"
+          icon-right="pencil"
+          @click="edit(item)"
+        />
       </div>
     </template>
+    <!-- Contacts -->
+    <!-- Start Modals -->
+    <b-modal
+      v-model="modalState"
+      :width="800"
+      :can-cancel="['x']"
+    >
+      <modal-form
+        :title="formTitle"
+        :form-data="formData"
+        @close="modalState = false"
+      ></modal-form>
+    </b-modal>
+    <!-- End Modals -->
   </div>
 </template>
 
@@ -19,21 +44,15 @@
 import { mapGetters } from 'vuex';
 
 export default {
+  components: {
+    ModalForm: () => import('@/components/Employee/Personal/Contact/ModalForm.vue'),
+  },
+
   data () {
     return {
-      isEmpty: false,
-      isStriped: true,
-      isHoverable: true,
-      hasMobileCards: true,
-      isPaginated: false,
-      isPaginationSimple: true,
-      isPaginationRounded: false,
-      paginationPosition: 'bottom',
-      defaultSortDirection: 'asc',
-      sortIcon: 'arrow-up',
-      sortIconSize: 'is-small',
-      currentPage: 1,
-      perPage: 10,
+      modalState: false,
+      formTitle: 'Add Contact',
+      formData: {},
     };
   },
 
@@ -41,6 +60,19 @@ export default {
     ...mapGetters({
       contacts: 'employee/selected/contacts',
     }),
+  },
+
+  methods: {
+    add () {
+      this.formTitle = 'Add Contact';
+      this.formData = {};
+      this.modalState = true;
+    },
+    edit (item) {
+      this.formTitle = 'Edit Contact';
+      this.formData = item;
+      this.modalState = true;
+    },
   },
 };
 </script>
